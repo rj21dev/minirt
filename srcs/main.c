d4a9fcb3c0c3519854bb5c9821419c6c1a061f9c
Chelsey Overandout <coverand@at-l7.msk.21-school.ru>
2022-07-04 23:35:56 +0300

    minor changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: coverand <coverand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:41:23 by rjada             #+#    #+#             */
-/*   Updated: 2022/07/04 19:10:25 by coverand         ###   ########.fr       */
+/*   Updated: 2022/07/04 22:48:04 by coverand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int	main(int argc, char **argv)
 	data->scene = scene;
 	render_scene(data);
 	mlx_key_hook(data->win_ptr, key_hook, (void *)data);
+	mlx_mouse_hook(data->win_ptr, mouse_hook, data);
 	mlx_hook(data->win_ptr, 17, 0, ft_close, (void *)data);
 	mlx_loop(data->mlx_ptr);
 	return (0);
